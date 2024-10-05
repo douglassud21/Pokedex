62cc3878da835a5c25086cd043f82415d91acef9
Douglas <douglas.nascimentosud21@gmail.com>
2024-10-05 18:10:16 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 
+
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?ofset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log (response)
-})
 
-console.log ("olá")
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (jsonBody) {
+        console.log (jsonBody)
+    })
+    .catch(function (error) {
+        console.error (error)
+    })
+    .finally(function () {
+        console.log ("Requisição concluída!")
+    })
