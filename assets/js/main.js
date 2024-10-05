5dabe1fc0cfa9fca3e432e51fcf229cc11be450a
Douglas <douglas.nascimentosud21@gmail.com>
2024-10-05 18:43:55 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,46 @@
 
+//Chamada da API.
 
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?ofset=${offset}&limit=${limit}`
 
 
+//Capturando os Pokemons da lista.
+
+function convertPokemonToLi (pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="/img/bulba-removebg-preview.png" alt="${pokemon.name}">
+
+                </div>
+            </li>
+            `
+}
+
+//Tratamento da URL .
+
 fetch(url)
-    .then(function (response) {
-        return response.json()
+    .then  ((response) => response.json())
+    .then  ((jsonBody) => jsonBody.results)
+    .then  ((pokemonList) => {
+
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log (convertPokemonToLi(pokemon))
+
+        }
+
     })
-    .then(function (jsonBody) {
-        console.log (jsonBody)
-    })
-    .catch(function (error) {
-        console.error (error)
-    })
-    .finally(function () {
-        console.log ("Requisição concluída!")
-    })
+    .catch ((error)    => console.error (error))
+
+
